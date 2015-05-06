080fe326b875dac24b52450e95863858be52f3fc
Diurno <Diurno@C7PC14>
2015-05-06 09:27:16 +0200

    Proyecto PruebaGIT modificado en el PaqueteDavidJimenez clase nueva

---
+++ src/PaqueteDavidJimenez/ClaseNuevaDavidJimenez.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PaqueteDavidJimenez;
+
+/**
+ *
+ * @author David Jimenez
+ */
+public class ClaseNuevaDavidJimenez {
+    String pruebas = "HOLA";
+}
