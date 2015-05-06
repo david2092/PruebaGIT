335af694b4e3530e7bc1832065eaf5b70eadaa99
Diurno <Diurno@C7PC14>
2015-05-06 10:08:01 +0200

    Proyecto PruebaGIT modificado en el PaqueteDavidJimenez clase nueva ModificadaPorAmbos

---
+++ src/PaqueteDavidJimenez/ModificadaPorAmbos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PaqueteDavidJimenez;
+
+/**
+ *
+ * @author David Jimenez
+ * @author Daniel Huergo
+ */
+public class ModificadaPorAmbos {
+    
+    public void escribir(){
+        System.out.println("Hola mundo");
+    }
+}
